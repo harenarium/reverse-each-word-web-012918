9f06712f696ee2c9d8d6d5c3c4719485e93466e4
Caroline Lee <carolinel2610@gmail.com>
2018-01-12 02:52:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do |x|
-    new_string << x.reverse
+    new_array << x.reverse
   end
   new_array.join(" ")
 end
